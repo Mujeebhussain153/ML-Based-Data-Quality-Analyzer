2e34c15d2e937427a5bd84bdfa87b8575da565a6
Mujeebhussain153 <mujeebhussain153@gmail.com>
2026-02-09 10:06:46 +0530

    Added Data loader code which takes csv data and loads it

---
+++ src/data_loader.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def load_data(self, file_path):
+        try:
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(f"File not found: {file_path}")
+            df = pd.read_csv(file_path)
+            return df
+        except Exception as e:
+            print(f"Error loading data: {e}")
+            return None
+
+
+if __name__ == "__main__":
+    loader = DataLoader()
+    df = loader.load_data(r"../data/yellow_tripdata_2015-01.csv")
+    print(df.head())
